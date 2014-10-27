e8c1ef22c06fb2b8549c49b95c245a477ef6f4f6
Elison Abreu <elisonwabreu@gmail.com>
2014-10-26 22:06:20 -0300

    adiconado funcao para tradução do calendario

--- WebContent/resources/js/scripts.js
+++ WebContent/resources/js/scripts.js
@@ -1,10 +1,37 @@
 function verificar(xhr, status, args, dlg, tbl) {
- if (args.validationFailed) {
-  PF(dlg).jq.effect("shake", {
-   times : 5
-  }, 100);
- } else {
-  PF(dlg).hide();
-  PF(tbl).clearFilters();
- }
+	if (args.validationFailed) {
+		PF(dlg).jq.effect("shake", {
+			times : 5
+		}, 100);
+	} else {
+		PF(dlg).hide();
+		PF(tbl).clearFilters();
+	}
 }
+
+PrimeFaces.locales['pt_BR'] = {
+	    closeText : 'Fechar',
+	    prevText : 'Anterior',
+	    nextText : 'Próximo',
+	    currentText : 'Começo',
+	    monthNames : [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+	    monthNamesShort : [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago','Set', 'Out', 'Nov', 'Des' ],
+	    dayNames : [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta','Sábado' ],
+	    dayNamesShort : [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+	    dayNamesMin : [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+	    weekHeader : 'Semana',
+	    firstDay : 1,
+	    isRTL : false,
+	    showMonthAfterYear : false,
+	    yearSuffix : '',
+	    timeOnlyTitle : 'Só Horas',
+	    timeText : 'Tempo',
+	    hourText : 'Hora',
+	    minuteText : 'Minuto',
+	    secondText : 'Segundo',
+	    ampm : false,
+	    month : 'Mês',
+	    week : 'Semana',
+	    day : 'Dia',
+	    allDayText : 'Todo Dia'
+	};
